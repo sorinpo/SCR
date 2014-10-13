f088fce425c7acf2c0990b1a560c31d1354123de
sorin <sorin.portase@gmail.com>
2014-10-14 00:42:21 +0300

    added version info to app

---
+++ scr-edu/src/main/java/sorinpo/scr/edu/controller/VersionController.java
@@ -0,0 +1,25 @@
+package sorinpo.scr.edu.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class VersionController {
+
+    private String version;
+
+    @ResponseBody
+    @RequestMapping(value = "/version", method = RequestMethod.GET, consumes = MediaType.ALL_VALUE, produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getVersion() {
+        return version;
+    }
+
+    @Value("project.version")
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
